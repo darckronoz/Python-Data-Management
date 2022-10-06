acb25c2716fd1b2c8727b3214e438661f20d9693
darckronoz <crispipe14@gmail.com>
2022-10-06 13:20:44 -0500

    taller conjuntos empezado

---
+++ conjuntos.py
@@ -0,0 +1,11 @@
+a="Hola Pythonista".upper()
+b=set(a)
+c=frozenset(a) #you cannot change a frozen set
+print(a)
+print(b)
+print(c)
+
+#Ejercicio asd
+#intersection_update(), isdisjoint(), isupperset(), 
+#pop(), remove(), symmetric_difference(), 
+# symmetric_difference_update() union() update()
