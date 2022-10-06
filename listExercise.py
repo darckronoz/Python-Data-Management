50edfb057bcf49857608c926f061e59dad782fb2
darckronoz <crispipe14@gmail.com>
2022-10-06 12:31:13 -0500

    taller 5 endedn added B)

--- listExercise.py
+++ listExercise.py
@@ -12,6 +12,9 @@
 #formato 3: -> function formatListThree
 #Empleado_1:Luis, Edad: 20, Peso: 55.6
 
+from re import S
+
+
 class employees:
     def __init__(self, name, age, weight):
         self.name = name
@@ -23,20 +26,29 @@ employeesList.append(employees('Lucia', 30, 65.8))
 employeesList.append(employees('Pedro', 18, 60))
 employeesList.append(employees('Luis', 20, 55.6))
 
-
-def formatListOne(xlist):
+# -> this uses pop so it empty the list xd *FIXEDnot.-.*
+def formatListOne(auxlist):
     index = 1
-    while(len(xlist) > 0):
-        employ = xlist.pop()
+    while(len(auxlist) > 0):
+        employ = auxlist.pop()
         print('empleado --', index)
         print(employ.name)
         print(employ.age)
         print(employ.weight)
         index+=1
 
-formatListOne(employeesList)
+#formatListOne(employeesList)
 
-#def formatListTwo(xlist):
+def formatListTwo(xlist):
+    for i in range(len(xlist)):
+        print('nombre empleado:', xlist[i].name, ', Edad:', xlist[i].age, ', Peso:', xlist[i].weight)
 
+formatListTwo(employeesList)
 
-#def formatListThree(xlist):
+def formatListThree(xlist):
+    index = 1
+    while(index <= len(xlist)):
+        print('Empleado_',index,':', xlist[index-1].name,', Edad:',xlist[index-1].age,', Peso:',xlist[index-1].weight)
+        index+=1
+
+formatListThree(employeesList)
