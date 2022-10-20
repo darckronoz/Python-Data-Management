0d07127a53b87bdc1da175edb5d5847a60c33c00
darckronoz <crispipe14@gmail.com>
2022-10-20 15:30:55 -0500

    lamda exercise

--- unidad 2/lambda1.py
+++ unidad 2/lambda1.py
@@ -1,6 +1,11 @@
 #lambda functions
 
 #example multiply by 2 a number (lambda function)
+from calendar import prmonth
+from curses import keyname
+from optparse import Values
+
+
 def mult2(n): return n*2 #example normal function in one line
 mult2l = lambda num : num*2 #example lambda function
 print(mult2(4))
@@ -16,7 +21,7 @@ def whisper(text):
 
 def greet(func):
     #storing the function in a variable
-    greeting = func("Hi, I am created by a function passed as an argument.")
+    greeting = func(f"Hi, I am created by a function passed as an argument.")
     print(greeting)
 
 greet(shout)
@@ -32,3 +37,58 @@ def apply_to_list(listx, function):
 print(apply_to_list(ints, lambda x: "even" if x%2 == 0 else "odd"))
 
 #to do: aporte revisar taller ultima parte
+
+#Lambda with arguments
+
+sampleO = lambda *args: sum(args) #with one it recieves a list of variables as arguments.
+print(sampleO(1, 4, 5, 6, 7))
+
+sampleDicts = lambda x, **args: print("si:", args.get(x)) #with two it recieves iterables.
+#you can use different types of arguments in one function
+sampleDicts('one', one='eluno', tre='sitres', dos='eldos')
+
+#decorators :0 this enable a function to implement the behavior of another function like so:
+
+def some_decorator(fuwu):
+    def wraps(*args):
+        print(f"Calling function '{fuwu.__name__}'")
+        return fuwu(args)
+    return wraps
+
+@some_decorator
+def decorated_function(x):
+    print(f"With argument '{x}'")
+
+decorated_function('argumentos de la segunda función')
+
+# Defining a decorator
+
+def trace(f):
+
+    def wrap(*args, **kwargs):
+
+        print(f"[TRACE] func: {f.__name__}, args: {args}, kwargs: {kwargs}")
+
+        return f(*args, **kwargs)
+
+
+    return wrap
+
+
+# Applying decorator to a function
+
+@trace
+
+def add_two(x):
+
+    return x + 2
+
+
+# Calling the decorated function
+
+add_two(3)
+
+
+# Applying decorator to a lambda
+
+print((trace(lambda x: x ** 2))(3))
