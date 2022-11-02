22980c7376ba554e287a3ac6d32fb2ec5a28a72f
darckronoz <crispipe14@gmail.com>
2022-11-02 14:52:21 -0500

    back up unidad 3 reboot*

--- unidad 3/NumPy.py
+++ unidad 3/NumPy.py
@@ -2,3 +2,14 @@
 #numpy se especializa en calculo numerico y analisis de datos en gran volumen
 #incorpora arrays 
 #procesar vectores o matrices es mucho mas rapido con numpy
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5]) #create numpy array
+print(a)
+
+print(a.shape) #prints a tuple with the dimensions of the array
+
+print(a.ndim) #Devuelve el número de dimensiones del array a.
+
+print(a.size) #Devuelve el número de elementos del array a.
+
