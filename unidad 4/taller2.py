38ff031f452b440af9d16ee128730aa2720cb889
darckronoz <crispipe14@gmail.com>
2022-12-04 17:41:54 -0500

    taller 2 ended

--- unidad 4/taller2.py
+++ unidad 4/taller2.py
@@ -1,8 +1,26 @@
-
-
-#change 
 import pandas as pd
 
-df_emp = pd.read_csv('/home/darckronoz/DEV/UPTC/Gestión_datos_python/Resources/employees.csv', error_bad_lines=False, sep=';', encoding='UTF-8')
+df_emp = pd.read_csv('../Resources/employees1.csv', error_bad_lines=False, sep=';', encoding='UTF-8')
 
-print(df_emp.info())
+# exercise 1 - COMMISSION PCT to numeric and reformat - ',' for '.'
+df_emp['COMMISSION_PCT'] = df_emp['COMMISSION_PCT'].str.replace(',', '.')
+df_emp['COMMISSION_PCT'] = pd.to_numeric(df_emp['COMMISSION_PCT'])
+df_emp['COMMISSION_PCT'] = df_emp['COMMISSION_PCT'].fillna(0)
+
+print(df_emp['COMMISSION_PCT'])
+
+# 2. Crear una nueva serie en el DataFrame empleados que calcule el salario por la comisión
+newSalary = df_emp['COMMISSION_PCT'] * df_emp['SALARY']
+df_emp['NEW_SALARY'] = newSalary
+
+print(df_emp[(df_emp.NEW_SALARY > 0)][['NEW_SALARY', 'FIRST_NAME']])
+
+
+# 3. Genere un nuevo DataFrame que muestre apellido, salario, fecha de contratación y comisión para los
+# empleados que hayan sido contratados en el segundo semestre del 2018 y que la comisión sea superior al
+# 25%.
+df_emp['HIRE_DATE'] = pd.to_datetime(df_emp['HIRE_DATE'])
+
+query = df_emp[(df_emp.COMMISSION_PCT > 0.25) & (pd.DatetimeIndex(df_emp.HIRE_DATE).year == 2018) & (pd.DatetimeIndex(df_emp.HIRE_DATE).month >= 6)][
+    ['FIRST_NAME', 'LAST_NAME', 'SALARY', 'HIRE_DATE', 'COMMISSION_PCT']]
+print(query)
